48946475f7cd4d8afd50244e41a950d391f3436b
gowsalyanatraj <pinkygowsi717@gmail.com>
2023-06-03 10:55:19 +0530

    chore

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme:
-          ThemeData(brightness: Brightness.light, primarySwatch: Colors.indigo),
+          ThemeData(brightness: Brightness.light,
+           primarySwatch:MaterialColor(0xFFFFE200, <int, Color>{
+       50: Color(0xFFE8EAF6),
+      100: Color(0xFFC5CAE9),
+      200: Color(0xFF9FA8DA),
+      300: Color(0xFF7986CB),
+      400: Color(0xFF5C6BC0),
+      600: Color(0xFF3949AB),
+      700: Color(0xFF303F9F),
+      800: Color(0xFF283593),
+      900: Color(0xFF1A237E),// correcting this error
+           })
+           ),
       darkTheme: ThemeData(brightness: Brightness.dark),
       themeMode: ThemeMode.system,
       debugShowCheckedModeBanner: false,
