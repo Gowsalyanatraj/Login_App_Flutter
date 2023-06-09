adfda3f168f3e566a746ff91413c5b5706acaa4d
gowsalyanatraj <pinkygowsi717@gmail.com>
2023-06-09 09:24:23 +0530

    setting up theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:login_app/src/utils/theme/theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,21 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme:
-          ThemeData(brightness: Brightness.light,
-           primarySwatch:MaterialColor(0xFFFFE200, <int, Color>{
-       50: Color(0xFFE8EAF6),
-      100: Color(0xFFC5CAE9),
-      200: Color(0xFF9FA8DA),
-      300: Color(0xFF7986CB),
-      400: Color(0xFF5C6BC0),
-      600: Color(0xFF3949AB),
-      700: Color(0xFF303F9F),
-      800: Color(0xFF283593),
-      900: Color(0xFF1A237E),// correcting this error
-           })
-           ),
-      darkTheme: ThemeData(brightness: Brightness.dark),
+      theme: GAppTheme.lightTheme,
+      darkTheme: GAppTheme.darkTheme,
       themeMode: ThemeMode.system,
       debugShowCheckedModeBanner: false,
       home: const AppHome(),
@@ -40,7 +28,7 @@ class AppHome extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.indigo.shade300,
+        backgroundColor: Colors.blue,
         title: const Text('.appable/'),
         leading: const Icon(Icons.ondemand_video),
       ),
