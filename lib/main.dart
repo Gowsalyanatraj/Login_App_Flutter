574b4b7b761a04fe4f8bedca2a41e2c45d446b69
gowsalyanatraj <pinkygowsi717@gmail.com>
2023-06-10 17:11:16 +0530

    chore

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class AppHome extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.lightBlue,
         title: const Text('.appable/'),
         leading: const Icon(Icons.ondemand_video),
       ),
