65f8e6afaa16a5fa73251e23f0044f79d5c8fea7
gowsalyanatraj <pinkygowsi717@gmail.com>
2023-06-12 09:39:03 +0530

    implementing google fonts

--- lib/src/utils/theme/theme.dart
+++ lib/src/utils/theme/theme.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class GAppTheme {
-  static  ThemeData lightTheme = ThemeData();
+  static  ThemeData lightTheme = ThemeData(
+    brightness: Brightness.light,
+    textTheme: TextTheme(
+      headline2: GoogleFonts.montserrat(
+        color: Colors.black87
+      ),
+      subtitle2: GoogleFonts.poppins(
+        color: Colors.black54,
+        fontSize: 24,
+      )
+    )
+  );
    static  ThemeData darkTheme = ThemeData();
 }
